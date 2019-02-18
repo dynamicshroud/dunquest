80f8d574bac29adefc5923239d1f4d2084be76c1
Not Virtual <virtual@cumallover.me>
2019-02-17 21:11:44 -0500

    added room creation

--- game.c
+++ game.c
@@ -1,6 +1,8 @@
 #include "game.h"
 #include <unistd.h>
 #include <stdio.h>
+#include <time.h> // for seed generation
+
 
 void heal(int life, int amount){
 	life = life + amount;
@@ -38,15 +40,50 @@ void create_savefile(int life, int strength, int maxweight){
 	fclose(f);
 }
 
-void read_savefile(){
+void log(const char* info){
+	puts(info);
+}
+
+void move_up(int y){
+	y = y + 1;
+}
+
+void move_down(int y){
+	y = y - 1;
+}
+
+void move_left(int x){
+	x = x - 1;
+}
+
+void move_right(int x){
+	x = x + 1;
+}
+
+struct room gen_room(){
+	/* rooms will always be rectangular to avoid more work :/ */
 	
-}
+	time_t seed;
+        srand((unsigned)time(&seed));
 
-struct room mkroom(){
-	struct room r;
-	r.x = 0;
-	r.y = 0;
-	r.type = std;
+        int rx = rand() % (23 + 1 - 3);
+        int ry = rand() % (13 + 1 - 3);
+        int addup = rand() % (6 + 1 - 1) + 1;
+
+        if(rx == 0) { rx = rx + addup; }
+        if(ry == 0) { ry = ry + addup; }
+
+	struct room r = {rx, ry, rx+2, ry+2};
 	return r;
+	
+
 }
 
+int iscollide(int x, int y){
+	// find position of player through parameters
+	// and detect if coords are equal to wall. if
+	// so, then return 1. if not, return 0.
+	
+	// TODO
+	return 0;
+}
